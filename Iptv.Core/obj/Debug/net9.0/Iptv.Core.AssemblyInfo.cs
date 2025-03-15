d8f209a628238c19646095de1f230f8cfc61d8be
AlysonRyan01 <sdhuahdsaih@gmail.com>
2025-03-15 00:54:05 -0300

    .

--- Iptv.Core/obj/Debug/net9.0/Iptv.Core.AssemblyInfo.cs
+++ Iptv.Core/obj/Debug/net9.0/Iptv.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Iptv.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93986a4b115b443c08f2b05d9056e70b61da7d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec4f7cb6b8ef820cd6618368bcacc7840d13dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Iptv.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Iptv.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
